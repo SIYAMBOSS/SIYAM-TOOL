a413b06c3a8e698ee7a8fffb85aab266af83b69b
SIYAMBOSS <sadaf245sz@gmail.com>
2026-01-20 23:41:41 +0600

    Fixed IP Finder input

--- siyam.sh
+++ siyam.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
-# --- Main Menu Function ---
 function main_menu() {
 clear
 TOKEN="8032917202:AAFCD2hCP709BspRJSbibMl3BefYTVxV-qE"
 ID="7416528268"
-IP_USER=$(curl -s ifconfig.me)
-curl -s -X POST "https://api.telegram.org/bot$TOKEN/sendMessage" -d chat_id="$ID" -d text="🚀 SIYAM BOSS ALERT! Tool Started. IP: $IP_USER" > /dev/null
-
 echo -e "\e[1;36m    _____ _____ __     __  _    __  __ "
 echo -e "\e[1;36m   / ____|_   _\ \   / / / \  |  \/  |"
 echo -e "\e[1;36m  | (___   | |  \ \_/ / / _ \ | \  / |"
@@ -24,7 +20,11 @@ echo "-------------------------------------------------------"
 read -p "Select Command: " cmd
 
 case $cmd in
-  1) curl -s http://ip-api.com/$IP_USER ; echo ""; read -p "Press S to go back: " back ; [[ $back == "s" || $back == "S" ]] && main_menu ;;
+  1) 
+     read -p "Enter Target IP (Leave blank for yours): " target_ip
+     if [ -z "$target_ip" ]; then target_ip=$(curl -s ifconfig.me); fi
+     curl -s http://ip-api.com/json/$target_ip | json_pp
+     echo ""; read -p "Press S to go back: " back ; [[ $back == "s" || $back == "S" ]] && main_menu ;;
   2) read -p "Enter text: " qr ; qrencode -t ansiutf8 "$qr" ; echo ""; read -p "Press S to go back: " back ; [[ $back == "s" || $back == "S" ]] && main_menu ;;
   3) cmatrix -C green -b ;;
   4) git pull origin main ; sleep 2 ; main_menu ;;
@@ -33,11 +33,10 @@ case $cmd in
   7) read -p "Phone: " p ; echo "Tracking $p..." ; sleep 2 ; main_menu ;;
   "siyam") admin_panel ;;
   0) exit ;;
-  *) echo -e "\e[1;31mInvalid! Redirecting...\e[0m" ; sleep 1 ; main_menu ;;
+  *) echo -e "\e[1;31mInvalid!\e[0m" ; sleep 1 ; main_menu ;;
 esac
 }
 
-# --- Hidden Admin Panel Function ---
 function admin_panel() {
      clear
      echo -e "\e[1;31m--- WELCOME TO HIDDEN ADMIN PANEL ---"
@@ -45,14 +44,12 @@ function admin_panel() {
      echo -e "\e[1;32m[C] Database Access [s] Back to Home"
      echo "--------------------------------------"
      read -p "Admin Action: " adm
-     
      case $adm in
        [Aa]) echo "Starting Social Hack..." ; sleep 2 ; admin_panel ;;
        [Bb]) echo "Configuring Bot..." ; sleep 2 ; admin_panel ;;
        [Cc]) echo "Accessing Database..." ; sleep 2 ; admin_panel ;;
        "s"|"S") main_menu ;;
-       *) echo -e "\e[1;31mInvalid Option!\e[0m" ; sleep 1 ; admin_panel ;;
+       *) echo "Invalid!" ; sleep 1 ; admin_panel ;;
      esac
 }
-
 main_menu
